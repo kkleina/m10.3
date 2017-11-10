d9cbbaa74dc95b82fe6ea32b2382c51bb1efc36c
Krzysztof Kleina <k.kleina@wp.eu>
2017-11-10 16:30:56 +0100

    Add control circles

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
 $(function() {
-    var carouselList = $('#carousel ul'),
-        leftButton = $('#js-leftButton'),
-        rightButton = $('#js-rightButton'),
-        firstItem = carouselList.find('li:first'),
-        lastItem = carouselList.find('li:last');
+    var carouselList = $('#carousel ul.slides'),
+        carouselControl = $('#carousel ul.control'),
+        prevButton = $('#js-leftButton'),
+        nextButton = $('#js-rightButton'),
+        currentSlide = 0;
     
     var interval = setInterval(changeSlideLeft, 5000);
-    firstItem.before(lastItem);
-    carouselList.css({marginLeft:-500});
+    moveLastSlide();
     
     function changeSlideLeft() {
         carouselList.animate({'marginLeft':-1000}, 500, moveFirstSlide);
+        currentSlide++;
+        current();
     }
     
     function changeSlideRight() {
         carouselList.animate({'marginLeft':0}, 500, moveLastSlide);
+        currentSlide--;
+        current();
     }
     
     function moveFirstSlide() {
@@ -31,6 +34,8 @@ $(function() {
         carouselList.css({marginLeft:-500});
     }
     
+// Stop and restart interval & prev/next buttons
+    
     function stopSlider() {
         clearInterval(interval);
     }
@@ -39,8 +44,43 @@ $(function() {
         interval = setInterval(changeSlideLeft, 5000);
     }
     
-    $('#js-leftButton').click(changeSlideLeft).hover(stopSlider, restartSlider);
+    prevButton.click(changeSlideRight).hover(stopSlider, restartSlider);
+    nextButton.click(changeSlideLeft).hover(stopSlider, restartSlider);
+    carouselControl.hover(stopSlider, restartSlider);
     
-    $('#js-rightButton').click(changeSlideRight).hover(stopSlider, restartSlider);
+// Active control circle    
+    
+    function current() {
+        if (currentSlide > 4) {
+            currentSlide = 0;
+        } else if (currentSlide < 0) {
+            currentSlide = 4;
+        }
+        
+        carouselControl.find('li').each(function(index, elem) {
+            if (carouselControl.find('li').index(elem) == currentSlide) {
+                carouselControl.find(elem).addClass('active');
+            } else {
+                carouselControl.find(elem).removeClass('active');
+            }
+        });
+    }
+
+// Change slide by control circles
+    
+    carouselControl.find('li').click(function() {
+        currentClick = $(this).index();
+        var diff = Math.abs(currentClick - currentSlide);
+        if (currentClick > currentSlide) {
+            for (i=0 ; i<diff ; i++) {
+                changeSlideLeft();
+            }
+        } else if (currentClick < currentSlide) {
+            for (i=0 ; i<diff ; i++) {
+                changeSlideRight();
+            }
+        }
+    });
+    
     
 });
